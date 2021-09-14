2f50267d79f89d399fc1484a0cadfdd3f38f155d
Mailen-Givre <mailen.givre@gmail.com>
2021-09-13 21:21:55 -0300

    Datos correctos

--- js/faltan.js
+++ js/faltan.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 13, 2021 15:37:25").getTime();
+var countDownDate = new Date("Nov 13, 2021 19:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
